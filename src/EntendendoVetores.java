a462923b9693312ca04a0d9b76bf64c1d7817cec
Fernando Bueno <fernando2583@gmail.com>
2021-03-05 20:15:12 -0300

    Aprendendo a usar vetores

---
+++ src/EntendendoVetores.java
@@ -0,0 +1,13 @@
+
+public class EntendendoVetores {
+
+	public static final int TAMANHO = 10;
+	public static void main(String[] args) {
+		int vetor[] = new int[TAMANHO];
+		
+		Scanner leitor = 
+		
+
+	}
+
+}
